e371be7fbb83e3fa473b26f1e5b27018a852b15e
Anuj Kumar <kumaranujam1009@gmail.com>
2024-04-09 11:42:16 +0530

    cors error fixed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const rootRouter = require("./routes/index")
 const cors = require('cors')
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: 'https://paytm-client.vercel.app',
+        methods: ['GET', 'POST'],
+        credentials: true,
+      },
+))
 app.use(express.json())
 
 app.use('/api/v1',rootRouter)
