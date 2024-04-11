e9a454fb14c5ccd621a3df871266c6257e9c0ebc
Anuj Kumar <kumaranujam1009@gmail.com>
2024-04-11 23:37:43 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const rootRouter = require("./src/routes/index")
 const cors = require('cors')
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:"http://paytmpe.vercel.app"
+}))
 app.use(express.json())
 app.use("/",(req,res)=>{
     res.send("Welcome to the Paytm Server")
