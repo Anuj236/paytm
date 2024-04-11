a66840f945b5571fc4438697144409d9e37d3fa9
Anuj Kumar <kumaranujam1009@gmail.com>
2024-04-11 12:05:30 +0530

    added client url

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const rootRouter = require("./routes/index")
 const cors = require('cors')
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:"https://paytm-client.vercel.app"
+}))
 app.use(express.json())
 app.use("/",(req,res)=>{
     res.send("Welcome to the Paytm Server")
