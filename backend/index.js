1f0fe531779457e6990d90e324490f5210568ad9
Anuj Kumar <kumaranujam1009@gmail.com>
2024-04-11 11:16:52 +0530

    vercel.json configured

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,12 @@ const rootRouter = require("./routes/index")
 const cors = require('cors')
 const app = express();
 
-app.use(cors(
-    {
-        origin: 'https://paytm-client.vercel.app',
-        methods: ['GET', 'POST'],
-        credentials: true,
-      },
-))
+app.use(cors())
 app.use(express.json())
+app.use("/",(req,res)=>{
+    res.send("Welcome to the Paytm Server")
 
+})
 app.use('/api/v1',rootRouter)
 
 app.listen(3000,()=>{
