360687b3463f8e07563be8b6f16d0978ec5aa44d
Anuj Kumar <kumaranujam1009@gmail.com>
2024-04-14 12:50:42 +0530

    added cors_origin

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const rootRouter = require("./src/routes/index")
 const cors = require('cors')
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true
+}))
 app.use(express.json())
 app.use('/api/v1',rootRouter)
 
